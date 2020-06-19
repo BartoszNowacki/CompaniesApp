58a97b8c415771cde0411dd05878b242b47a6b51
Bartosz Nowacki <bartoszn@yanosik.pl>
2020-06-19 15:41:12 +0200

    -initial commit

---
+++ CompaniesApp/CompaniesApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CompaniesApp
+//
+//  Created by Bartosz Nowacki on 19/06/2020.
+//  Copyright © 2020 Bartosz Nowacki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
